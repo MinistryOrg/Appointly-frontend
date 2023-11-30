e816579b497847504cdd9bbcc2cac294e3460ad9
coccalis <kokkalisxristos@gmail.com>
2023-11-30 14:01:30 +0200

    setup project

---
+++ appointly/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Button, ButtonGroup } from "@nextui-org/react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button color="primary">Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
