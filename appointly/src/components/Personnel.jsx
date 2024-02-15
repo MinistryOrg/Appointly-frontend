bc5e3e3260f182085929b6bfb97d7ae76467af6e
coccalis <kokkalisxristos@gmail.com>
2024-02-16 00:15:56 +0200

    missed function

--- appointly/src/components/Personnel.jsx
+++ appointly/src/components/Personnel.jsx
@@ -1,11 +1,13 @@
 import { Card, CardFooter, Image, Spinner } from "@nextui-org/react";
 import { useAppointment } from "../contexts/AppointmentContext";
 import { useShops } from "../contexts/ShopContext";
+import { getImageURL } from "../utils/image-util";
 
 function Personnel() {
   const { setSelectedPersonnel, selectedPersonnel } = useAppointment();
   const { currentShop } = useShops();
   const { personnel } = currentShop;
+
   return (
     <div>
       <div className="h-auto my-unit-3xl xsm:mx-unit-2xl lg:mx-unit-0 flex xsm:flex-col lg:flex-row justify-center sm:gap-unit-md lg:gap-unit-5xl xsm:gap-y-unit-xl">
@@ -23,7 +25,7 @@ function Personnel() {
                   alt="service1"
                   className="object-cover"
                   height={250}
-                  src="../../src/styles/images/person.svg"
+                  src={getImageURL("person.svg")}
                   width={280}
                 />
                 <CardFooter className="justify-between  overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)]  ml-1 z-10">
